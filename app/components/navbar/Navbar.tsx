015f554a52d830501a2377e57491e9af6245626f
karar <karar@KARAPC>
2024-02-20 23:53:54 -0500

    2/20/2024 Navbar on progress

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import Container from "../Container";
+
+const Navbar = () => {
+return (
+
+    <div className="fixed w-full bg-white z-10  shadow-sm">
+<div className="
+py-5
+boarder-b[1px]
+">
+
+<Container>
+Helloo
+</Container>
+
+</div>
+
+    </div>
+);
+
+}
+export default Navbar;
